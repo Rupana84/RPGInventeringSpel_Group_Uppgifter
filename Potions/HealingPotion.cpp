309a8e4b6305795198942ee103188f5c616fb298
Stephanie Johansson <3tomu24.johast@folkuniversitetet.nu>
2025-11-03 15:03:51 +0100

    Add stack vs heap allocation examples to potion use methods

--- Potions/HealingPotion.cpp
+++ Potions/HealingPotion.cpp
@@ -4,4 +4,14 @@ HealingPotion::HealingPotion() : Potion("Healing Potion", 50) {}
 
 void HealingPotion::use() {
     std::cout << "You used a " << getName() << " and restored " << getValue() << " health points!" << std::endl;
+
+    // Demonstrating stack allocation
+    int temporaryHealth = getValue(); // stack-object
+    std::cout << "Temporary healing value on stack: " << temporaryHealth << std::endl;
+
+    // Demonstrating heap allocation
+    int* heapHealth = new int(getValue()); // heap-object
+    std::cout << "Temporary healing value on heap: " << *heapHealth << std::endl;
+
+    delete heapHealth; // Delete heap-object to prevent memory leak
 }
--- Potions/ManaPotion.cpp
+++ Potions/ManaPotion.cpp
@@ -4,4 +4,14 @@ ManaPotion::ManaPotion() : Potion("Mana Potion", 30) {}
 
 void ManaPotion::use() {
     std::cout << "You used a " << getName() << " and restored " << getValue() << " mana points!" << std::endl;
+
+    // Demonstrating stack allocation
+    int temporaryMana = getValue(); // stack-object
+    std::cout << "Temporary mana value on stack: " << temporaryMana << std::endl;
+
+    // Demonstrating heap allocation
+    int* heapMana = new int(getValue()); // heap-object
+    std::cout << "Temporary mana value on heap: " << *heapMana << std::endl;
+    
+    delete heapMana; // Delete heap-object to prevent memory leak
 }
